779a5892a615525e258e32bb6437edbd575a05f8
Salo Valdez <salovaldez.net@gmail.com>
2025-08-28 22:59:43 -0500

    creandop repo

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $(".btnFotoActa").click(function(){
+
+    generar_actaImagen();
+   });
+
+
+   function generar_actaImagen(){
+
+      
+    $objetivo = document.querySelector("#acta");
+    $contenedorCanvas = document.querySelector("#contenedor_img_acta"); // En dónde ponemos el elemento canvas
+
+    html2canvas($objetivo) // Llamar a html2canvas y pasarle el elemento
+    .then(canvas => {
+            // Cuando se resuelva la promesa traerá el canvas
+            $contenedorCanvas.appendChild(canvas); // Lo agregamos como hijo del div
+            let enlace = document.createElement('a');
+
+      
+            // Convertir la imagen a Base64
+            enlace.href = canvas.toDataURL();
+            // Hacer click en él
+            enlace.click();
+      });
+    }
+
+
+
+});
