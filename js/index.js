a64b08bae55e0571ddb414e4df881add220d61f5
Salo Valdez <salovaldez.net@gmail.com>
2025-09-10 06:18:38 -0500

    subiendoi 2

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+
+
+
+const today = new Date()
+today.toLocaleDateString() // de acuerdo a tu idioma local
+ // formato fecha en Colombia, por ejemplo 9/1/2021
+
+
+$("#txtFecha").val(today.toLocaleDateString("es-CO"));
+
   $(".btnFotoActa").click(function(){
 
     generar_actaImagen();
@@ -14,7 +24,7 @@ $(document).ready(function(){
     html2canvas($objetivo,{
        dpi: window.devicePixelRatio*1,
         scale:1.5,
-        width:1100,
+        width:990,
         heigth:1800,
 
     }) // Llamar a html2canvas y pasarle el elemento
@@ -23,7 +33,9 @@ $(document).ready(function(){
             $contenedorCanvas.appendChild(canvas); // Lo agregamos como hijo del div
             let enlace = document.createElement('a');
 	          //  window.devicePixelRatio();
-      
+      var aleatorio = Math.random()
+                // return false;
+                enlace.download = aleatorio+"."+"png"
             // Convertir la imagen a Base64
             enlace.href = canvas.toDataURL();
             // Hacer click en él
